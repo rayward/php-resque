2e0229b44d9aae119680f0f1f1db8e194e0d7114
Ray Ward <ray@rayward.com.au>
2017-04-10 14:50:40 -0700

    style

--- config/config.php
+++ config/config.php
@@ -5,23 +5,23 @@ use Psr\Log\LoggerInterface;
 use Resque\Reserver;
 
 return [
-    ContainerInterface::class         => function (ContainerInterface $c) {
+    ContainerInterface::class => function (ContainerInterface $c) {
         return $c;
     },
 
-    'env.redis.host'                  => DI\env('REDIS_BACKEND', null),
-    'env.redis.db'                    => DI\env('REDIS_BACKEND_DB', null),
-    'env.redis.prefix'                => DI\env('PREFIX', ''),
-    'env.app_include'                 => DI\env('APP_INCLUDE', null),
-    'env.worker.count'                => DI\env('COUNT', 1),
-    'env.worker.check_interval'       => DI\env('INTERVAL', 5),
-    'env.worker.pidfile'              => DI\env('PIDFILE', null),
-    'env.blpop.timeout'               => DI\env('BLPOP_TIMEOUT', DI\get('env.worker.check_interval')),
-    'env.reserver'                    => DI\env('RESERVER', null),
-    'env.reserver.use_blocking'       => DI\env('BLOCKING', null),
-    'env.queue'                       => DI\env('QUEUE', null),
+    'env.redis.host'            => DI\env('REDIS_BACKEND', null),
+    'env.redis.db'              => DI\env('REDIS_BACKEND_DB', null),
+    'env.redis.prefix'          => DI\env('PREFIX', ''),
+    'env.app_include'           => DI\env('APP_INCLUDE', null),
+    'env.worker.count'          => DI\env('COUNT', 1),
+    'env.worker.check_interval' => DI\env('INTERVAL', 5),
+    'env.worker.pidfile'        => DI\env('PIDFILE', null),
+    'env.blpop.timeout'         => DI\env('BLPOP_TIMEOUT', DI\get('env.worker.check_interval')),
+    'env.reserver'              => DI\env('RESERVER', null),
+    'env.reserver.use_blocking' => DI\env('BLOCKING', null),
+    'env.queue'                 => DI\env('QUEUE', null),
 
-    'queues'                      => function (ContainerInterface $c) {
+    'queues' => function (ContainerInterface $c) {
         $queues = $c->get('env.queue');
         if (empty($queues)) {
             return false;
