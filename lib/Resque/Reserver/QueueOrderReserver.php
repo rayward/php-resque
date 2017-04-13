0551698ec03481886a0afd37fcb0682b40a5ec44
Ray Ward <ray@rayward.com.au>
2017-04-13 09:48:08 -0700

    prototype

---
+++ lib/Resque/Reserver/QueueOrderReserver.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Resque\Reserver;
+
+use Resque_Job;
+
+/**
+ * QueueOrderReserver reserves jobs in the order that the queues is given. This is the default reserver.
+ * For example: given queues A, B and C, all the jobs from queue A will be processed before moving onto queue B and
+ * then after that queue C.
+ */
+class QueueOrderReserver extends AbstractReserver implements ReserverInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function reserve()
+    {
+        foreach ($this->getQueues() as $queue) {
+            $this->logger->debug("[{reserver}] Checking queue '{queue}' for jobs", array(
+                'queue'    => $queue,
+                'reserver' => $this->getName(),
+            ));
+
+            $job = Resque_Job::reserve($queue);
+            if ($job) {
+                $this->logger->info("[{reserver}] Found job on queue '{queue}'", array(
+                    'queue'    => $queue,
+                    'reserver' => $this->getName(),
+                ));
+                return $job;
+            }
+        }
+
+        return null;
+    }
+}
---
+++ lib/Resque/Reserver/ReserverInterface.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Resque\Reserver;
+
+use Resque_Job;
+
+interface ReserverInterface
+{
+    /**
+     * Gets the queues to reserve jobs from.
+     *
+     * @return array
+     */
+    public function getQueues();
+
+    /**
+     * Reserves a job.
+     *
+     * @return Resque_Job|null A job instance or null if not job was available to reserve.
+     */
+    public function reserve();
+
+    /**
+     * If there was no job available to reserve, should the worker wait before attempting to reserve a job again?
+     *
+     * @return bool
+     */
+    public function waitAfterReservationAttempt();
+
+    /**
+     * Gets a friendly name of this reserver.
+     *
+     * @return string
+     */
+    public function getName();
+}
